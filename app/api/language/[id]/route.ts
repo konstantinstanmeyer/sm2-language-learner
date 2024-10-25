92ceef9133b4869a04eff6ed2b5dc512577ac298
KonstantinStanmeyer <konstantin@stanmeyer.com>
2024-10-25 15:32:15 -0400

    route GET handler fix test 2. for production

--- app/api/language/[id]/route.ts
+++ app/api/language/[id]/route.ts
@@ -5,7 +5,7 @@ interface Params {
     id: string;
 }
 
-export async function GET(request: NextRequest, { params }: {params: Params}){
+export async function GET(request: NextRequest, { params }: {params: Promise<{ id: string }> }){
     const { id } = await params;
 
     const text = await fs.readFile(process.cwd() + "/lib/languages/" + id + "/basic.txt", 'utf-8');
